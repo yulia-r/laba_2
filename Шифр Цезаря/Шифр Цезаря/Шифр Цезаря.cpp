774177a7c4559445e7422f0ee966eb4891d0e7be
yulia-r <56191525+yulia-r@users.noreply.github.com>
2020-09-18 18:50:17 +0600

    Первая версия программы

---
+++ Шифр Цезаря/Шифр Цезаря/Шифр Цезаря.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian"); //возможность ввести текст на русском
+	cout << "Введите текст для обработки: " << endl; //указания
+	string s; int k;
+	getline(cin, s);
+	cout << "Введите шаг сдвига (*Примечание: при дешифровке шаг сдвига указывается отрицательным): " << endl; //указания
+	cin >> k;
+
+	for (auto& c : s)
+	{
+		if (c >= 'a' && c <= 'z')
+			c = ((c - 'a' + k) % 26) + 'a';
+		else if (c >= 'A' && c <= 'Z')
+			c = ((c - 'A' + k) % 26) + 'A';
+	}
+
+	cout << s << endl;
+}
